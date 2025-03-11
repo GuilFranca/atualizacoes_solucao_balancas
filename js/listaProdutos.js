f3d03ac0d9e9ce541ae853e1ade65a2417c514ab
Guilherme França Fernandes <guilfranca27@gmail.com>
2025-03-11 14:52:43 -0300

    inserção de produtos pela função funcionando normalmente junto da paginação

--- js/listaProdutos.js
+++ js/listaProdutos.js
@@ -5,9 +5,9 @@ const listaProdutos = [
         preco: "4.250,00",
         categoria: "açougue",
         subcategoria: "AMACIADORES DE BIFE",
-        maisVendidos: "sim",
+        maisProcurados: "sim",
+        vezes: "",
         promocao: "",
-        vezes: ""
     },
     {
         nome: 'faca inox desossa 6" 5515-06 5500 mundial',
@@ -15,9 +15,9 @@ const listaProdutos = [
         preco: "52,00",
         categoria: "açougue",
         subcategoria: "FACAS",
-        maisVendidos: "sim",
+        maisProcurados: "sim",
+        vezes: "",
         promocao: "",
-        vezes: ""
     },
     {
         nome: 'PICADOR DE CARNES GURAL MGI-10',
@@ -25,19 +25,19 @@ const listaProdutos = [
         preco: "3.690,07",
         categoria: "açougue",
         subcategoria: "PICADORES DE CARNES",
-        maisVendidos: "sim",
+        maisProcurados: "sim",
+        vezes: "",
         promocao: "",
-        vezes: ""
     },
     {
-        nome: 'Alho',
-        cod: "",
-        preco: "",
-        categoria: "",
-        subcategoria: "",
-        maisVendidos: "",
-        promocao: "",
-        vezes: ""
+        nome: 'AUTO SERVIÇO 3 PORTAS ASFL 2000',
+        cod: "85",
+        preco: "14.400,00",
+        categoria: "Refrigeração",
+        subcategoria: "Auto Serviço",
+        maisProcurados: '',
+        vezes: "12,990 x6",
+        promocao: "10.990,00",
     },
 ];
 
@@ -55,220 +55,120 @@ listaProdutos.sort((a, b) => {
     return 0; // Nomes iguais
 });
 
-// Exibir a lista ordenada no console
-console.log(listaProdutos);
-
-let filtroProdutos = [...listaProdutos];
 const cardsPorPagina = 20;
-let paginaInicial = 1;
-
+let paginaAtual = 1;
 
 function mostrarProdutos(pagina) {
-    const inicio = (pagina - 1) * cardsPorPagina;
-    const fim = inicio + cardsPorPagina;
+    const secaoCardTitulo = document.querySelector('.secaoCard__titulo').textContent.trim().toLowerCase();
 
-    // Usar filtroProdutos em vez de listaProdutos
-    const produtosParaMostrar = filtroProdutos.slice(inicio, fim);
+    let produtosFiltrados;
 
-    const secaoCard = document.querySelector('.secaoCard__main');
-    secaoCard.innerHTML = ''; // Limpar o conteúdo atual antes de adicionar novos produtos
-
-    produtosParaMostrar.forEach(produto => {
-        const nome = produto.nome;
-        const cod = produto.cod;
-        const preco = produto.preco;
-        const categoria = produto.categoria;
-
-        // Adicionar o card do produto
-        secaoCard.innerHTML += `
-            <!-- Card starts -->
-            <div class="cardProduto">
-                <img src="../images_produtos/${cod}.png" alt="${nome}" class="cardProduto_img">
-                <h1 class="cardProduto_descricao">${nome}</h1>
-                <p class="cardProduto_cod">Cod: ${cod}</p>
-                <p class="cardProduto_preco">R$${preco} à vista</p>
-                <a href="../pages_produtos/${cod}.html" class="cardProduto_link">Saiba Mais</a>
-                <p class='categoria' hidden>${categoria}</p>
-            </div>
-            <!-- Card ends -->
-        `;
-    });
-
-    // Atualizar a paginação
-    gerarPaginacao(pagina);
-}
-
-// Função para gerar a paginação
-function gerarPaginacao(pagina) {
-    const totalProdutos = filtroProdutos.length;
-    const totalPaginas = Math.ceil(totalProdutos / cardsPorPagina);
-
-    if (totalPaginas === 1) {
-        return
+    if (secaoCardTitulo === 'promoções do mês') {
+        // Filtra apenas produtos que estão em promoção
+        produtosFiltrados = listaProdutos.filter(produto => produto.promocao.trim() !== '');
+    } else if (secaoCardTitulo === 'mais procurados') {
+        // Filtra apenas produtos mais vendidos
+        produtosFiltrados = listaProdutos.filter(produto => produto.maisProcurados !== '');
     } else {
-        const secaoPaginacao = document.querySelector('.paginacao');
-        secaoPaginacao.innerHTML = ''; // Limpar a seção de paginação antes de adicionar novos botões
-
-        // Botão "Anterior"
-        const prevButton = document.createElement('button');
-        prevButton.classList.add('btn__pagination');
-        prevButton.innerHTML = `<i class="ri-arrow-left-double-line"></i>`;
-        prevButton.disabled = pagina === 1; // Desabilitar o botão "Anterior" se estiver na primeira página
-        prevButton.addEventListener('click', () => mostrarProdutos(pagina - 1));
-        secaoPaginacao.appendChild(prevButton);
-
-        // Botões de número de páginas
-        for (let i = 1; i <= totalPaginas; i++) {
-            const pageButton = document.createElement('button');
-            pageButton.classList.add('btn__pagination');
-            pageButton.textContent = i;
-            pageButton.classList.toggle('ativo', i === pagina); // Destacar a página atual
-            pageButton.addEventListener('click', () => mostrarProdutos(i));
-            secaoPaginacao.appendChild(pageButton);
-        }
-
-        // Botão "Próximo"
-        const nextButton = document.createElement('button');
-        nextButton.classList.add('btn__pagination');
-        nextButton.innerHTML = '<i class="ri-arrow-right-double-fill"></i>';
-        nextButton.disabled = pagina === totalPaginas; // Desabilitar o botão "Próximo" se estiver na última página
-        nextButton.addEventListener('click', () => mostrarProdutos(pagina + 1));
-        secaoPaginacao.appendChild(nextButton);
-
-        // Função para rolar suavemente ao topo
-        function scrollToTop() {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        }
-
-        scrollToTop();
+        // Filtra produtos por categoria ou subcategoria
+        produtosFiltrados = listaProdutos.filter(produto =>
+            produto.categoria.toLowerCase() === secaoCardTitulo ||
+            produto.subcategoria.toLowerCase() === secaoCardTitulo
+        );
     }
 
-
-}
-
-// Função pesquisa
-const inputPesquisa = document.querySelector('.input__barra__pesquisa');
-const btnPesquisa = document.querySelector('.button__barra__pesquisa');
-const secaoMaisProcurados = document.querySelector('.secaoMaisProcurados');
-
-// Verificar se há um termo de pesquisa salvo ao carregar a página
-// window.addEventListener('load', () => {
-//     const termoSalvo = localStorage.getItem('termoPesquisa');
-
-//     if (termoSalvo) {
-//         // Preencher o campo de pesquisa com o termo salvo
-//         inputPesquisa.value = termoSalvo;
-
-//         // Aplicar a pesquisa automaticamente
-//         pesquisa();
-//     } else {
-//         // Se não houver termo salvo, mostrar todos os produtos
-//         filtroProdutos = [...listaProdutos];
-//         mostrarProdutos(paginaInicial);
-//         gerarPaginacao(paginaInicial);
-//     }
-// });
-
-btnPesquisa.addEventListener('click', function () {
-    pesquisa();
-});
-
-inputPesquisa.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        pesquisa();
-    }
-});
-
-function pesquisa() {
-    const valorFiltro = inputPesquisa.value.toLowerCase();
-
-    // Salvar o termo de pesquisa no localStorage
-    localStorage.setItem('termoPesquisa', valorFiltro);
-
-    if (valorFiltro === '') {
-        secaoMaisProcurados.innerHTML = `
-        <h1 class="secaoCard__titulo">Pesquisa</h1>
-        <hr class="secaoCard__hr" />
-
-        <!-- Container de cards -->
-        <div class="secaoCard__main"></div>
-
-        <!-- Paginação -->
-        <ul id="paginacao" class="paginacao"></ul>
-    `
-    } else {
-        // Atualizar o título da seção de pesquisa
-        secaoMaisProcurados.innerHTML = `
-        <h1 class="secaoCard__titulo">${valorFiltro}</h1>
-        <hr class="secaoCard__hr" />
-
-        <!-- Container de cards -->
-        <div class="secaoCard__main"></div>
-
-        <!-- Paginação -->
-        <ul id="paginacao" class="paginacao"></ul>
-    `
-    }
-
-    // Filtrar produtos com base no nome ou código
-    filtroProdutos = listaProdutos.filter(produto =>
-        produto.nome.toLowerCase().includes(valorFiltro) ||
-        produto.cod.toLowerCase().includes(valorFiltro)
-    );
-
-    // Mostrar os produtos filtrados
-    mostrarProdutos(paginaInicial);
-    gerarPaginacao(pagina);
-}
-
-function mostrarCategoriaProdutos(pagina) {
-    // Captura o título da seção e normaliza o texto
-    const filtro = document.querySelector('.secaoCard__titulo').textContent.trim().toLowerCase();
-
-    if (!filtro) {
-        console.warn("Nenhuma categoria ou subcategoria encontrada no título.");
-        return;
-    }
-
-    // Filtrar produtos que pertencem à categoria OU à subcategoria correspondente
-    const produtosFiltrados = listaProdutos.filter(produto => 
-        produto.categoria.toLowerCase() == filtro || produto.subcategoria.toLowerCase() == filtro
-    );
-
+    const totalProdutos = produtosFiltrados.length;
     const inicio = (pagina - 1) * cardsPorPagina;
     const fim = inicio + cardsPorPagina;
     const produtosParaMostrar = produtosFiltrados.slice(inicio, fim);
 
     const secaoCard = document.querySelector('.secaoCard__main');
-    secaoCard.innerHTML = ''; // Limpar antes de adicionar novos produtos
+    secaoCard.innerHTML = ''; // Limpa a seção antes de adicionar novos produtos
 
-    // Se não houver produtos, exibir mensagem
+    // Se não houver produtos, exibir uma mensagem
     if (produtosParaMostrar.length === 0) {
-        secaoCard.innerHTML = "<p>Nenhum produto encontrado nesta categoria ou subcategoria.</p>";
+        secaoCard.innerHTML = `<p>Nenhum produto encontrado.</p>`;
         return;
     }
 
     produtosParaMostrar.forEach(produto => {
-        const { nome, cod, preco, categoria, subcategoria } = produto;
+        const estaEmPromocao = produto.promocao.trim() !== ''; // Verifica se há promoção
+        const precoClasse = estaEmPromocao ? "promotion__color" : ""; // Adiciona classe condicionalmente
 
-        // Adicionar o card do produto
-        secaoCard.innerHTML += `
+        if (secaoCardTitulo === 'Mais Procurados') {
+            secaoCard.innerHTML += `
             <div class="cardProduto">
-                <img src="../images_produtos/${cod}.png" alt="${nome}" class="cardProduto_img">
-                <h1 class="cardProduto_descricao">${nome}</h1>
-                <p class="cardProduto_cod">Cod: ${cod}</p>
-                <p class="cardProduto_preco">R$${preco} à vista</p>
-                <a href="../pages_produtos/${cod}.html" class="cardProduto_link">Saiba Mais</a>
-                <p class='categoria' hidden>${categoria}</p>
-                <p class='subcategoria' hidden>${subcategoria}</p>
+                <img src="./images_produtos/${produto.cod}.png" alt="${produto.nome}" class="cardProduto_img">
+                <h1 class="cardProduto_descricao">${produto.nome}</h1>
+                <p class="cardProduto_cod">Cod: ${produto.cod}</p>
+                <p class="cardProduto_preco ${precoClasse}">
+                    ${estaEmPromocao ? `
+                        <span class="preco-antigo">R$${produto.vezes}</span> <br> 
+                        <span class="preco-novo">R$${produto.promocao} à vista</span>
+                    ` : `
+                        R$${produto.preco} à vista
+                    `}
+                </p>
+                <a href="./pages_produtos/${produto.cod}.html" class="cardProduto_link">Saiba Mais</a>
             </div>
         `;
+        } else {
+            secaoCard.innerHTML += `
+            <div class="cardProduto">
+                <img src="../images_produtos/${produto.cod}.png" alt="${produto.nome}" class="cardProduto_img">
+                <h1 class="cardProduto_descricao">${produto.nome}</h1>
+                <p class="cardProduto_cod">Cod: ${produto.cod}</p>
+                <p class="cardProduto_preco ${precoClasse}">
+                    ${estaEmPromocao ? `
+                        <span class="preco-antigo">R$${produto.vezes}</span> <br> 
+                        <span class="preco-novo">R$${produto.promocao} à vista</span>
+                    ` : `
+                        R$${produto.preco} à vista
+                    `}
+                </p>
+                <a href="../pages_produtos/${produto.cod}.html" class="cardProduto_link">Saiba Mais</a>
+            </div>
+        `;
+        }
+
     });
 
-    // Atualizar a paginação
-    gerarPaginacao(pagina);
+    // Atualiza a paginação corretamente
+    gerarPaginacao(pagina, totalProdutos);
 }
 
-// Chamar a função ao carregar a página
-mostrarCategoriaProdutos(paginaInicial);
 
+
+// Atualiza a paginação corretamente com base nos produtos filtrados
+function gerarPaginacao(pagina, totalProdutos) {
+    const totalPaginas = Math.ceil(totalProdutos / cardsPorPagina);
+    const secaoPaginacao = document.querySelector('.paginacao');
+    secaoPaginacao.innerHTML = '';
+
+    if (totalPaginas <= 1) return;
+
+    const botaoAnterior = document.createElement('button');
+    botaoAnterior.classList.add('btn__pagination');
+    botaoAnterior.innerHTML = '<i class="ri-arrow-left-double-line"></i>';
+    botaoAnterior.disabled = pagina === 1;
+    botaoAnterior.addEventListener('click', () => mostrarProdutos(pagina - 1));
+    secaoPaginacao.appendChild(botaoAnterior);
+
+    for (let i = 1; i <= totalPaginas; i++) {
+        const botaoPagina = document.createElement('button');
+        botaoPagina.classList.add('btn__pagination');
+        botaoPagina.textContent = i;
+        if (i === pagina) botaoPagina.classList.add('ativo');
+        botaoPagina.addEventListener('click', () => mostrarProdutos(i));
+        secaoPaginacao.appendChild(botaoPagina);
+    }
+
+    const botaoProximo = document.createElement('button');
+    botaoProximo.classList.add('btn__pagination');
+    botaoProximo.innerHTML = '<i class="ri-arrow-right-double-line"></i>';
+    botaoProximo.disabled = pagina === totalPaginas;
+    botaoProximo.addEventListener('click', () => mostrarProdutos(pagina + 1));
+    secaoPaginacao.appendChild(botaoProximo);
+}
+
+mostrarProdutos(1);
